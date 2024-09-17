84f69dc2e5d6d153240e81a6ecd15940390a4ca2
Teemu Kallio <Teemu.Kallio3@metropolia.fi>
2024-09-17 15:48:07 +0300

    Initial

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
 
 public class TemperatureConverter {
+
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public boolean isExtremeTemperature(double celsius) {
+        return celsius < -40 || celsius > 50;
+    }
+
+    public static float kelvinToCelsius(float kelvin) {
+        return (float) (kelvin - 273.15);
+    }
 }
--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TemperatureConverterTest {
+
+    private TemperatureConverter converter;
+
+    @Before
+    public void setUp() {
+        converter = new TemperatureConverter();
+    }
+
+    @Test
+    public void testFahrenheitToCelsius() {
+        assertEquals(0, converter.fahrenheitToCelsius(32), 0.01);
+        assertEquals(100, converter.fahrenheitToCelsius(212), 0.01);
+        assertEquals(-40, converter.fahrenheitToCelsius(-40), 0.01);
+    }
+
+    @Test
+    public void testCelsiusToFahrenheit() {
+        assertEquals(32, converter.celsiusToFahrenheit(0), 0.01);
+        assertEquals(212, converter.celsiusToFahrenheit(100), 0.01);
+        assertEquals(-40, converter.celsiusToFahrenheit(-40), 0.01);
+    }
+
+    @Test
+    public void testIsExtremeTemperature() {
+        assertTrue(converter.isExtremeTemperature(-50));
+        assertFalse(converter.isExtremeTemperature(20));
+        assertTrue(converter.isExtremeTemperature(60));
+    }
+
+    @Test
+    public void testKelvinToCelsius() {
+        assertEquals(0.0, converter.kelvinToCelsius(273.15F), 0.01);
+    }
 }
+
